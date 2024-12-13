7ec3c1e179ce7dfc25b7ebb267fb2e1ec7af668b
jeffreyjczy <94435580+jeffreyjczy@users.noreply.github.com>
2024-12-13 18:19:36 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/jeffreyjczy.github.io",
+  site: "https://jeffreyjczy.github.io",
   base: "blog",
 });
